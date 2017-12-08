bd0616cfc18db2d36d1425c553c468218420c069
Daniël <daniel@burst-digital.com>
2017-12-08 15:49:44 +0100

    Update importById to function correctly

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,13 +2,17 @@
 
 namespace Drupal\mage_ninja\Controller;
 
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityStorageException;
 use Drupal\mage_ninja\Api\Api;
 use Drupal\mage_ninja\Api\JsonExceptionResponse;
 use Drupal\mage_ninja\Api\SearchCriteriaBuilder;
+use Drupal\mage_ninja\DataModel\Product;
 use Drupal\mage_ninja\Entity\MageNinjaProduct;
 use Drupal\mage_ninja\Batch\Import;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -32,7 +36,7 @@ class ProductController extends ControllerBase {
       /** @var array $authHeader */
       $authHeader = Api::getAuthHeader($token);
 
-      $endpoint = 'V1/products/id/' . $id;
+      $endpoint = 'V1/mage_ninja/product/' . $id;
       $options = [
         'headers' => $authHeader
       ];
@@ -40,11 +44,11 @@ class ProductController extends ControllerBase {
       /** @var \GuzzleHttp\Psr7\Response $response */
       $response = $client->get($endpoint, $options);
 
-      /** @var \Symfony\Component\Serializer\SerializerInterface $serializer */
+      /** @var \Symfony\Component\Serializer\Encoder\DecoderInterface $serializer */
       $serializer = \Drupal::service('serializer');
 
       /** @var MageNinjaProduct $product */
-      $product = $serializer->deserialize($response->getBody(), MageNinjaProduct::class, 'json');
+      $product = $serializer->decode($response->getBody(), 'json');
 
       return new JsonResponse($product);
     } catch (RequestException $e) {
@@ -154,29 +158,85 @@ class ProductController extends ControllerBase {
    * @param int $id
    *  The reference id (Magento product ID)
    *
-   * @return JsonExceptionResponse|RedirectResponse
+   * @return JsonExceptionResponse|JsonResponse
    */
   public function importById($id) {
+    /** @var \Symfony\Component\Serializer\Encoder\DecoderInterface $decoder */
+    $decoder = \Drupal::service('serializer');
+
+    /** @var int|array $productEntityId */
+    $productEntityIds = \Drupal::entityQuery('mage_ninja_product')
+      ->condition('reference_id', $id)
+      ->execute();
+
+    if(sizeOf($productEntityIds) === 1) {
+      // Get actual ID from array where the ID is the key
+      $productEntityId = reset($productEntityIds);
+
+    } else if(sizeOf($productEntityIds) > 1){
+      \Drupal::logger('mage_ninja')->error('Multiple MageNinjaProduct entities exist for reference_id: ' . $id . '.');
+
+      return new JsonResponse([
+        'success' => false
+      ]);
+    }
+    
     try {
-      /** @var \Drupal\Core\Entity\Query\QueryInterface $productEntity */
-      $productEntity = \Drupal::entityQuery('mage_ninja_product')
-        ->condition('reference_id', $id)
-        ->execute();
-
-      if (empty($productEntity)) {
-        /** @var MageNinjaProduct $productEntity */
-        $productEntity = MageNinjaProduct::create([
-          'reference_id' => $id
-        ]);
-        $productEntity->save();
-      }
-
-      \Drupal::logger('mage_ninja')->info('Import: product with ID: \'' . $id . '\' imported.');
-
-      return new RedirectResponse('/');
+      /** @var JsonResponse $itemJson */
+      $itemJson = $this->getById($id);
     } catch (RequestException $e) {
       return new JsonExceptionResponse($e);
     }
+
+    /** @var array $item */
+    $item = $decoder->decode($itemJson->getContent(), 'json');
+
+    /** @var Product $product */
+    $product = new Product($item);
+
+    if(empty($productEntityId)) {
+      // This MageNinjaProduct entity does not yet exist, so create a new one.
+
+      MageNinjaProduct::create($product->toArray())->save();
+
+      \Drupal::logger('mage_ninja')->info('Import: product with ID: \'' . $id . '\' imported.');
+    } else {
+      // Update properties for this MageNinjaProduct entity.
+
+      /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager */
+      $entityTypeManager = \Drupal::service('entity_type.manager');
+
+      try {
+        /** @var \Drupal\mage_ninja\Entity\MageNinjaProductInterface $productEntity */
+        $productEntity = $entityTypeManager
+          ->getStorage('mage_ninja_product')
+          ->load($productEntityId);
+      } catch(InvalidPluginDefinitionException $e) {
+        \Drupal::logger('mage_ninja')->error($e->getMessage());
+
+        return new JsonExceptionResponse($e);
+      }
+
+      $productEntity->set('sku', $product->getSku());
+      $productEntity->set('price', $product->getPricing()->getPrice());
+      $productEntity->set('special_price', $product->getPricing()->getSpecialPrice());
+      $productEntity->set('special_price_from', $product->getPricing()->getSpecialPriceFrom());
+      $productEntity->set('special_price_to', $product->getPricing()->getSpecialPriceTo());
+
+      try {
+        $productEntity->save();
+      } catch (EntityStorageException $e) {
+        \Drupal::logger('mage_ninja')->error($e->getMessage());
+
+        return new JsonExceptionResponse($e);
+      }
+
+      \Drupal::logger('mage_ninja')->info('Import: product with ID: \'' . $id . '\' updated.');
+    }
+
+    return new JsonResponse([
+      'success' => true
+    ]);
   }
 
   /**
