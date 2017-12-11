9bfb5c7ea08a2832aabd03dd878ef1935e62bf41
Daniël <daniel@burst-digital.com>
2017-12-11 10:56:27 +0100

    Create OAuthController

---
+++ src/Controller/OAuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\mage_ninja\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class OAuthController extends ControllerBase {
+  public function identity() {
+
+  }
+
+  public function callback() {
+    
+  }
+}
