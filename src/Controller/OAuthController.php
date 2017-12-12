7c32259cd09f5b28a15c5ff14eaa122016e23fdd
Daniël <daniel@burst-digital.com>
2017-12-12 17:36:33 +0100

    Rename variables

--- src/Controller/OAuthController.php
+++ src/Controller/OAuthController.php
@@ -7,19 +7,20 @@ use GuzzleHttp\Psr7\Response;
 
 class OAuthController extends ControllerBase {
   public function callback() {
-    // Data received from Magento
-    $oauth_consumer_key = $_POST['oauth_consumer_key'];
-    $oauth_consumer_secret = $_POST['oauth_consumer_secret'];
-    $store_base_url = $_POST['store_base_url'];
-    $oauth_verifier = $_POST['oauth_verifier'];
+    // This function is called by Magento before the user is sent to the OAuthForm.
+
+    $oauthConsumerKey = $_POST['oauth_consumer_key'];
+    $oauthConsumerSecret = $_POST['oauth_consumer_secret'];
+    $storeBaseUrl = $_POST['store_base_url'];
+    $oauthVerifier = $_POST['oauth_verifier'];
 
     /** @var \Drupal\Core\Config\Config $config */
     $config = \Drupal::service('config.factory')->getEditable('mage_ninja.settings');
 
-    $config->set('oauth_consumer_key', $oauth_consumer_key)->save();
-    $config->set('oauth_consumer_secret', $oauth_consumer_secret)->save();
-    $config->set('oauth_verifier', $oauth_verifier)->save();
-    $config->set('store_base_url', $store_base_url)->save();
+    $config->set('oauth_consumer_key', $oauthConsumerKey)->save();
+    $config->set('oauth_consumer_secret', $oauthConsumerSecret)->save();
+    $config->set('oauth_verifier', $oauthVerifier)->save();
+    $config->set('store_base_url', $storeBaseUrl)->save();
 
     return new Response();
   }
